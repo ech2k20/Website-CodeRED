fc66787dcbb1563c53957b74e6ab4de9e255c78f
ech2k20 <ech2447@gmail.com>
2021-10-07 15:15:52 -0500

    changes

--- src/components/pages/GetStarted.js
+++ src/components/pages/GetStarted.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function GetStarted() {
-  return <h1 className='get-started'>SAFETY AND SHIT</h1>;
+  return <h1 className='get-started'>SAFETY</h1>;
 }
